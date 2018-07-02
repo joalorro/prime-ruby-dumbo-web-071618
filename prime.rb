89d0475d1935843da1a5109fee3e875f9d580016
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 16:00:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,18 @@
+require "pry"
+
 def prime?(int)
   arr = []
+  
   for m in 2 .. Math.sqrt(int.abs) do
     arr.push m % int 
   end
   
+  binding.pry
+  
   arr.each do |num|
-    
     if num == 0
       return false       
     end
-    
   end
   
   return true 
