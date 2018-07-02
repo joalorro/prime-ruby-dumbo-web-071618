2f5971ac98d0548aa239bc29b73ae4fb102d0352
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 16:18:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,14 @@
 require "pry"
 
 def prime?(int)
-  arr1 = []
-  arr2 = [] 
+  arr = Array.new
   range = 2 .. Math.sqrt(int.abs)
   
   for m in range do
-    arr2.push m
-    
-    arr1.push (int % m )
+    arr.push (int % m )
   end
   
-  arr1.each do |num|
+  arr.each do |num|
     if num == 0
       return false       
     end
