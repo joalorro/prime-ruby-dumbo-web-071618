4806d981599497ea00342104fcbf507ab177e946
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 16:20:13 +0000

    automatically backed up by learn

---
+++ primeTest.rb
@@ -0,0 +1,19 @@
+def prime?(Array)
+  newArr = Array.new
+  range = 2 .. Math.sqrt(int.abs)
+  
+  for m in range do
+    newArr.push (int % m )
+  end
+  
+  newArr.each do |num|
+    if num == 0
+      return false       
+    end
+  end
+  
+  return true 
+  
+end
+
+prime?(27)
